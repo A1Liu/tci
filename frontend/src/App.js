f51b0af7052058f8a76637885f1865f83b9d0191
Evan Silverman <es4753@nyu.edu>
2020-11-02 09:57:34 -0500

    some small styling changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div>
       <ul className="flex h-10 px-2 py-2">
         <li className="mr-6">
-          <a className="text-blue-500 hover:text-blue-800" href="/">
+          <a className="text-blue-500 hover:text-blue-800 font-sans" href="/">
             TCI
           </a>
         </li>
@@ -39,7 +39,7 @@ function App() {
           <p> {message} </p>
         </div>
         {/* Will replace with Split Component later */}
-        <div className="w-full md:w-4/5 bg-gray-500 p-4 text-center text-gray-200">
+        <div className="w-full md:w-4/5 bg-gray-500 p-0 text-center text-gray-200">
           <div className="flex h-screen md:flex-row flex-wrap">
             <div className="w-full md:w-1/2 bg-orange-800 p-4 text-center text-brown-700">
               Editor
