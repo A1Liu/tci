e1a1c9896f746d3ba5d47b9d8ebd86586ca26b45
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-11-01 23:19:03 -0500

    Add basic layout and file upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
 import { useEffect, useState } from "react";
+import "./tailwind.css";
+import FileUpload from "./components/fileUpload";
 
 function App() {
   const [socket, setSocket] = useState(undefined);
@@ -15,23 +16,40 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <button
-          type="button"
-          onClick={() => {
-            socket.send(JSON.stringify({ command: "AddFile", data: {} }));
-          }}
-        >
-          Hello
-        </button>
-
-        <p> {message} </p>
-      </header>
+    <div>
+      <ul className="flex h-10 px-2 py-2">
+        <li className="mr-6">
+          <a className="text-blue-500 hover:text-blue-800" href="/">
+            TCI
+          </a>
+        </li>
+      </ul>
+      <div className="flex h-screen md:flex-row flex-wrap">
+        <div className="w-full md:w-1/5 bg-gray-400 p-4 text-center text-gray-700">
+          <FileUpload />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="button"
+            onClick={() => {
+              socket.send(JSON.stringify({ command: "AddFile", data: {} }));
+            }}
+          >
+            Hello
+          </button>
+          <p> {message} </p>
+        </div>
+        {/* Will replace with Split Component later */}
+        <div className="w-full md:w-4/5 bg-gray-500 p-4 text-center text-gray-200">
+          <div className="flex h-screen md:flex-row flex-wrap">
+            <div className="w-full md:w-1/2 bg-orange-800 p-4 text-center text-brown-700">
+              Editor
+            </div>
+            <div className="w-full md:w-1/2 bg-orange-900 p-4 text-center text-black-200">
+              Terminal
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/fileUpload.js
+++ frontend/src/components/fileUpload.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FileUpload() {
-  return <div />;
+  const [files, setFiles] = useState([]);
+
+  const handleOnUpload = (event) => {
+    setFiles(event.target.files);
+    console.log(files);
+  };
+
+  return (
+    <div className="pt-2 pb-5">
+      <input type="file" multiple onChange={handleOnUpload} />
+    </div>
+  );
 }
