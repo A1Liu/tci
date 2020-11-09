05783c016ffc8d55a33706293f7eae5f9183ae02
Santiago Rendon <sr4670@nyu.edu>
2020-11-08 23:24:00 -0500

    added basics for browser terminal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./tailwind.css";
+import "./App.css";
 import FileUpload from "./components/fileUpload";
+import Terminal from "./components/terminal";
 
 function App() {
   const [socket, setSocket] = useState(undefined);
@@ -44,8 +46,11 @@ function App() {
             <div className="w-full md:w-1/2 bg-orange-800 p-4 text-center text-brown-700">
               Editor
             </div>
-            <div className="w-full md:w-1/2 bg-orange-900 p-4 text-center text-black-200">
-              Terminal
+            <div
+              id="terminal-div"
+              className="w-full md:w-1/2 bg-orange-900 p-4 text-center text-black-200"
+            >
+              <Terminal />
             </div>
           </div>
         </div>
--- frontend/src/components/terminal.js
+++ frontend/src/components/terminal.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Terminal() {
-  return <div />;
+function caretToggle() {
+  const caret = document.getElementsByClassName("term-caret")[0];
+  if (caret.classList.contains("blink")) {
+    caret.classList.remove("blink");
+  } else {
+    caret.classList.add("blink");
+  }
 }
+
+function logKey(e) {
+  const terminalText = document.querySelector("#terminal-text");
+  const character = `${String.fromCharCode(e.keyCode)}`;
+  const result = terminalText.childNodes[0];
+  if (e.keyCode === 13) {
+    result.nodeValue += "\nROOT$ ";
+  } else if (e.keyCode === 8) {
+    // dont delete if no characters inputted
+    if (result.nodeValue.length > 7) {
+      result.nodeValue = result.nodeValue.substring(
+        0,
+        result.nodeValue.length - 1
+      );
+    }
+  } else {
+    result.nodeValue += character;
+    console.log(character);
+  }
+}
+
+class Terminal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", logKey);
+    setInterval(caretToggle, 500);
+  }
+
+  render() {
+    return (
+      <div>
+        <p id="terminal-title">Terminal</p>
+        <p id="terminal-text">
+          ROOT$
+          <span className="term-caret">&#x2588;</span>
+        </p>
+      </div>
+    );
+  }
+}
+export default Terminal;
