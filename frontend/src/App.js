92689b93396191be319e9dafac4896df3df2d077
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-11-14 22:26:09 -0500

    Finish Editor and minor modification to styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,30 @@ import "./tailwind.css";
 import "./App.css";
 import FileUpload from "./components/fileUpload";
 import Terminal from "./components/terminal";
-import Editor from "./components/editor";
+import BasicEditor from "./components/editor";
 
 function App() {
   return (
     <div>
-      <ul className="flex h-10 px-2 py-2">
+      <ul className="flex h-10 px-2 py-2 bg-gray-800 border-b border-gray-600">
         <li className="mr-6">
-          <a className="text-blue-500 hover:text-blue-800 font-sans" href="/">
+          <a className="text-blue-500 hover:text-white font-sans" href="/">
             TCI
           </a>
         </li>
       </ul>
-      <div className="flex h-screen md:flex-row flex-wrap">
-        <div className="w-full md:w-1/6 bg-gray-600 p-4 text-center text-white">
+      <div className="flex min-h-screen flex-wrap">
+        <div className="w-full md:w-1/6 bg-gray-800 p-4 text-center text-white">
           <FileUpload />
         </div>
-        {/* Will replace with Split Component later */}
-        <div className="h-screen w-full md:w-5/6 bg-gray-500 p-0 text-center text-gray-200">
+        <div className="md:w-5/6 p-0 text-center text-gray-200">
           <div className="flex h-full md:flex-row flex-wrap">
-            <div className="w-full md:w-1/2 bg-gray-800 text-center border-solid border border-gray-600">
-              <Editor />
+            <div className="w-full md:w-1/2 text-center border-r border-gray-800">
+              <BasicEditor />
             </div>
             <div
               id="terminal-div"
-              className="w-full md:w-1/2 bg-gray-800 p-4 text-center border-solid border-2 border-opacity-25 border-gray-600"
+              className="w-full md:w-1/2 text-center border-solid"
             >
               <Terminal />
             </div>
