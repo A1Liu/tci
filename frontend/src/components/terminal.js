c0cf2c07bb1f1919088f25927b9a7e1abf6c9895
Santiago Rendon <sr4670@nyu.edu>
2020-11-16 02:49:12 -0500

    added unwind

--- frontend/src/components/terminal.js
+++ frontend/src/components/terminal.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
-let distanceFromNewLine = 6;
+const output = [[]];
+let lineNum = 0;
 
 function caretToggle() {
   const caret = document.getElementsByClassName("term-caret")[0];
@@ -11,25 +12,69 @@ function caretToggle() {
   }
 }
 
+function unwind(num) {
+  const terminalText = document.querySelector("#terminal-text");
+  const result = terminalText.childNodes[0];
+  let newNum = num;
+  // dont let unwind if nothing to unwind
+  if (result.nodeValue.length <= 6) {
+    result.nodeValue = "root$ ";
+    return;
+  }
+  // delete entire line if unwind is large enough
+  if (num > output[lineNum].length) {
+    newNum -= output[lineNum].length;
+    result.nodeValue = result.nodeValue.substring(
+      0,
+      result.nodeValue.length - output[lineNum].length - 7
+    );
+    // do not remove last entry in output
+    if (output.length > 1) {
+      output.pop();
+      lineNum -= 1;
+    } else {
+      output[lineNum] = [];
+    }
+    // if there is more characters to delete continue deletion
+    if (newNum >= 0) {
+      unwind(newNum);
+    }
+  } else if (output[lineNum].length > 0) {
+    result.nodeValue = result.nodeValue.substring(
+      0,
+      result.nodeValue.length - newNum
+    );
+    output[lineNum] = output[lineNum].slice(0, -newNum);
+  }
+}
+
 function logKey(e) {
   const terminalText = document.querySelector("#terminal-text");
   const character = `${String.fromCharCode(e.keyCode)}`.toLowerCase();
   const result = terminalText.childNodes[0];
   if (e.keyCode === 13) {
-    result.nodeValue += "\nroot$ ";
-    distanceFromNewLine = 6;
+    // enter pressed
+    let command = output[lineNum].join("");
+    command = command.replace(/\s/g, ""); // remove spaces
+    if (command === "unwind") {
+      unwind(5 + 6);
+    } else {
+      lineNum += 1;
+      output.push([]);
+      result.nodeValue += "\nroot$ ";
+    }
   } else if (e.keyCode === 8) {
-    // dont delete if no characters inputted
-    if (distanceFromNewLine > 6) {
+    // dont delete if there is nothing to delete
+    if (output[lineNum].length > 0) {
+      output[lineNum].pop();
       result.nodeValue = result.nodeValue.substring(
         0,
         result.nodeValue.length - 1
       );
-      distanceFromNewLine -= 1;
     }
   } else {
+    output[lineNum].push(character);
     result.nodeValue += character;
-    distanceFromNewLine += 1;
   }
 }
 
