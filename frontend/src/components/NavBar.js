5b27780c4d3fae7b00fb759fe44047cc9d1cfb6f
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-12-02 23:55:34 -0500

    Fix for input div

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -91,7 +91,12 @@ export default function NavBar() {
       </div>
       <div className="ml-5 pt-1">
         <button type="button" onClick={handleCheckBox}>
-          <input type="checkbox" id="replay" checked={replay} />
+          <input
+            type="checkbox"
+            id="replay"
+            checked={replay}
+            onChange={handleCheckBox}
+          />
           <span className="ml-1 text-white">Replay</span>
         </button>
       </div>
