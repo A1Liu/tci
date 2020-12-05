c9a614e8588897dfaa2b350d66fe34d5bd44e4f9
Khanh Nguyen <44149581+Kn99HN@users.noreply.github.com>
2020-12-05 16:06:30 -0500

    Change controller's button styles (#53)
    * Change controller's button styles
    
    * Change margin

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import styled from "styled-components";
 import { useFileUpload } from "./fileUploadContext";
 
 export default function NavBar() {
@@ -73,66 +74,105 @@ export default function NavBar() {
   }
 
   return (
-    <nav className="h-12 w-full bg-gray-800 flex p-2 border-b border-gray-600">
-      <div className="pb-5 ml-2">
-        <input
-          style={{ display: "none" }}
-          type="file"
-          ref={hiddenFileInput}
-          onChange={handleOnChange}
-        />
-        <button
-          className="bg-blue-600 hover:bg-blue-800 text-white py-1 px-2 rounded"
-          type="button"
-          onClick={handleOnClick}
-        >
-          Upload a File
-        </button>
-      </div>
-      <div className="ml-5 pt-1">
-        <button type="button" onClick={handleCheckBox}>
-          <input
-            type="checkbox"
-            id="replay"
-            checked={replay}
-            onChange={handleCheckBox}
-          />
-          <span className="ml-1 text-white">Replay</span>
-        </button>
-      </div>
-      {replay ? (
-        <>
-          <button
-            className="bg-blue-600 hover:bg-blue-800 text-white text-bold py px-4 rounded ml-8"
-            type="button"
-            onClick={moveBackward}
-          >
-            Prev
-          </button>
-          <button
-            className="bg-blue-600 hover:bg-blue-800 text-white text-bold py px-4 rounded ml-8"
-            type="button"
-            onClick={compile}
-          >
-            Run
-          </button>
-          <button
-            className="bg-blue-600 hover:bg-blue-800 text-white text-bold py px-4 rounded ml-8"
-            type="button"
-            onClick={moveForward}
-          >
-            Next
-          </button>{" "}
-        </>
-      ) : (
-        <button
-          className="bg-blue-600 hover:bg-blue-800 text-white text-bold py px-4 rounded ml-8"
-          type="button"
-          onClick={compile}
-        >
-          Play
-        </button>
-      )}
-    </nav>
+    <div className="w-full bg-gray-800 border-b border-gray-600">
+      <nav className="h-12 w-1/2 justify-between flex p-2 border-r border-gray-600">
+        {replay ? (
+          <div className="flex justify-between mt-1 w-40">
+            <StepButton left onClick={moveBackward}>
+              <Vertical left />
+              <Arrow left />
+            </StepButton>
+            <PlayButton onClick={compile} />
+            <StepButton type="button" onClick={moveForward}>
+              <Arrow />
+              <Vertical />
+            </StepButton>
+          </div>
+        ) : (
+          <PlayButton onClick={compile} />
+        )}
+        <div className="flex">
+          <div className="mr-2 pt-1">
+            <button type="button" onClick={handleCheckBox}>
+              <input
+                type="checkbox"
+                id="replay"
+                checked={replay}
+                onChange={handleCheckBox}
+              />
+              <span className="ml-1 text-white">Replay</span>
+            </button>
+          </div>
+          <div className="pb-5 ml-2">
+            <input
+              style={{ display: "none" }}
+              type="file"
+              ref={hiddenFileInput}
+              onChange={handleOnChange}
+            />
+            <button
+              className="bg-blue-600 hover:bg-blue-800 text-white py-1 px-2 rounded"
+              type="button"
+              onClick={handleOnClick}
+            >
+              Upload a File
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
+
+const PlayButton = styled.button`
+  width: 0;
+  height: 24px;
+  border-style: solid;
+  border-color: transparent transparent transparent #d3d3d3;
+  box-sizing: border-box;
+  border-width: 12px 0px 12px 18px;
+  transition: 200ms all ease;
+  margin: 0 10px;
+  &:hover {
+    border-color: transparent transparent transparent #808080;
+  }
+`;
+
+const Vertical = styled.span`
+  width: 0;
+  height: 24px;
+  border-style: solid;
+  border-color: ${({ left }) =>
+    left
+      ? "transparent #d3d3d3 transparent transparent"
+      : "transparent transparent transparent #d3d3d3"};
+  box-sizing: border-box;
+  border-width: ${({ left }) =>
+    left ? "0px 5px 0px 15px" : "0px 15px 0px 5px"};
+`;
+
+const StepButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  margin: 0 5px;
+  transition: 200ms all ease;
+  &:hover ${Vertical} {
+    border-color: ${({ left }) =>
+      left
+        ? "transparent #808080 transparent transparent"
+        : "transparent transparent transparent #808080"};
+  }
+`;
+
+const Arrow = styled.span`
+  width: 0;
+  height: 24px;
+  border-style: solid;
+  border-color: transparent transparent transparent #d3d3d3;
+  box-sizing: border-box;
+  border-width: 12px 0px 12px 18px;
+  transform: ${({ left }) => (left ? "rotate(180deg)" : "rotate(0)")};
+  ${StepButton}:hover & {
+    border-color: transparent transparent transparent #808080;
+  }
+`;
