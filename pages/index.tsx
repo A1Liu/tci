201b4c2c55d5a6e87f6bad4590e2d0d830a8f012
Albert Liu <albertymliu@gmail.com>
2023-04-25 11:22:59 -0700

    clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import SplitPane from "react-split-pane";
 import styles from "./tci.module.css";
-import BasicEditor from "@/components/Editor";
-import NavBar from "@/components/NavBar";
-import Terminal from "@/components/Terminal";
 
 export function App() {
   return (
@@ -21,34 +17,6 @@ export function App() {
         <a className={styles.name} href="https://github.com/A1Liu/tci">
           Check out TCI on Github!
         </a>
-      </div>
-
-      <NavBar />
-
-      <div style={{ position: "relative", flexGrow: 1 }}>
-        <SplitPane
-          split="vertical"
-          defaultSize="50%"
-          minSize={200}
-          maxSize={-200}
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <BasicEditor />
-          </div>
-
-          <div style={{ width: "100%", height: "100%" }}>
-            <Terminal />
-          </div>
-        </SplitPane>
       </div>
     </div>
   );
