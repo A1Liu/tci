76b2cc7b3cfa573978e19bbd70805dad4844f20b
Albert Liu <albertymliu@gmail.com>
2023-04-26 15:28:43 -0700

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,78 @@ pub mod api {
     pub use std::collections::HashMap;
 
     #[cfg(debug_assertions)]
+    pub use super::run_test_code;
+
+    #[cfg(debug_assertions)]
     pub use serde::{Deserialize, Serialize};
 
     #[cfg(test)]
     pub use ntest::*;
+}
+
+const TEST_CASE_DELIMITER: &'static str = "// -- END TEST CASE --\n";
+
+#[derive(serde::Serialize)]
+pub struct PipelineOutput<'a> {
+    #[serde(skip_serializing)]
+    source: &'a str,
+    lexer: Vec<lexer::TokenKind>,
+}
+
+impl<'a> PipelineOutput<'a> {
+    pub fn test_case(&self) -> String {
+        let mut output = self.source.to_string();
+
+        let text = serde_json::to_string(self).expect("failed to serialize test output");
+
+        if !output.ends_with("\n") {
+            output.push('\n');
+        }
+
+        output.push_str(TEST_CASE_DELIMITER);
+        output.push_str(&text);
+        output.push('\n');
+
+        return output;
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct PipelineInput {
+    lexer: Option<Vec<lexer::TokenKind>>,
+}
+
+#[cfg(debug_assertions)]
+pub fn run_test_code(test_source: &str) -> PipelineOutput {
+    use crate::api::*;
+
+    let (source, expected_str) = test_source
+        .split_once(TEST_CASE_DELIMITER)
+        .unwrap_or((test_source, "null"));
+
+    let expected = serde_json::from_str::<Option<PipelineInput>>(expected_str)
+        .expect("Test case expected value didn't parse")
+        .unwrap_or(PipelineInput { lexer: None });
+
+    let mut source_string = source.to_string();
+    if !source_string.ends_with("\n") {
+        source_string.push('\n');
+    }
+
+    let mut files = FileDb::new();
+    let file_id = files
+        .add_file("main.c".to_string(), source_string)
+        .expect("file should add properly");
+    let file = &files.files[file_id as usize];
+
+    let res = lex(&files, file).expect("Expected lex to succeed");
+
+    if let Some(expected) = &expected.lexer {
+        assert_eq!(&res.tokens.kind, expected, "Invalid token stream");
+    }
+
+    return PipelineOutput {
+        source,
+        lexer: res.tokens.kind,
+    };
 }
---
+++ src/tests/lexer/include.c
@@ -0,0 +1,4 @@
+#include <stdbool.h>
+int main() {}
+// -- END TEST CASE --
+{"lexer":["Typedef","Char","Ident","Semicolon","Newline","Hashtag","Ident","Ident","PreprocessingNum","Newline","Hashtag","Ident","Ident","PreprocessingNum","Newline","Int","Ident","LParen","RParen","LBrace","RBrace","Newline"]}
