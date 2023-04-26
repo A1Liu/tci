3d03ee7e63556f0c4df126a3b68da56824a9bc47
Albert Liu <albertymliu@gmail.com>
2023-04-26 15:52:22 -0700

    meh

--- old/lib/test/statics/statics.c
+++
@@ -1,8 +0,0 @@
-#include "statics.h"
-#include <stdio.h>
-
-static int private_function() {
-  printf("Hello from private function in statics.c!\n");
-}
-
-void public_function() { private_function(); }
