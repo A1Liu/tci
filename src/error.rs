635c41d67e4eab6fe1ddf792aec32207f36e4cce
Albert Liu <albertymliu@gmail.com>
2023-04-29 01:35:22 -0700

    meh

--- src/error.rs
+++ src/error.rs
@@ -3,3 +3,12 @@ pub struct Error {
     pub message: String,
     pub start: u32,
 }
+
+impl Error {
+    pub fn new(s: &str, start: u32) -> Error {
+        return Error {
+            message: s.to_string(),
+            start,
+        };
+    }
+}
