18c1db472a336abd90ab8a9ba361de136303ee15
Albert Liu <albertymliu@gmail.com>
2023-05-06 22:01:26 -0700

    meh

--- compiler-tests/parser/lexer/dotdot.c
+++ compiler-tests/parser/lexer/dotdot.c
@@ -2,4 +2,4 @@ int main() {
     ..
 }
 // -- END TEST CASE --
-// {"lexer":{"Err":{"InvalidCharacterSequence":{"seq":"..","index":17}}},"macro_expansion":{"Err":"DidntRun"},"parsed_ast":{"Err":"DidntRun"}}
+// {"lexer":{"Err":{"kind":{"InvalidCharacterSequence":{"seq":"..","index":17}}}},"macro_expansion":"Ignore","parsed_ast":"Ignore","ast_validation":"Ignore"}
