76745e35196e40b0056af30a6626eb69557ffa0a
Albert Liu <albertymliu@gmail.com>
2023-05-10 14:37:37 -0700

    meh

--- compiler/src/walrus_codegen.rs
+++ compiler/src/walrus_codegen.rs
@@ -1,12 +1,12 @@
-use walrus::ValType;
+use walrus::*;
 
 use crate::api::*;
 
 pub fn codegen(ast: &AstNodeVec) -> Vec<u8> {
-    let config = walrus::ModuleConfig::new();
-    let mut module = walrus::Module::with_config(config);
+    let config = ModuleConfig::new();
+    let mut module = Module::with_config(config);
 
-    let func = walrus::FunctionBuilder::new(&mut module.types, &[], &[ValType::I32])
+    let func = FunctionBuilder::new(&mut module.types, &[], &[ValType::I32])
         .finish(vec![], &mut module.funcs);
 
     return module.emit_wasm();
